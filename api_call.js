24e13a32ae6c16371e68394c2534495621f14e2e
raimoncoding <robertjanstefan@gmail.com>
2024-02-07 07:43:34 +0100

    Delete api_call.js

--- api_call.js
+++
@@ -1,4 +0,0 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json()) // Convert the response to JSON
-  .then(data => console.log(data)) // Do something with the data
-  .catch(error => console.error('An error occurred:', error));
